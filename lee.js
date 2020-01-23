29c28aea1aaeddc75d5697734aead3ca9aeb4400
Med-Said <mohamedsaidmbareck@gmail.com>
2020-01-23 23:51:23 +0100

    print path

--- lee.js
+++ lee.js
@@ -29,14 +29,29 @@ function ArrayDeque()
     }
 }
 
+//show path
+function printPath( node) {
+    if (node == undefined) {
+        return;
+    }
+    printPath(node.parent);
+    document.write(node.show());
+}
 
 //Lee algorithm
 
-function Node(x, y, dist) {
+    // maintain a parent node for printing path
+    //Node parent;
+
+function Node(x, y, dist, parent) {
     this.x = x || 0;
     this.y = y || 0;
     this.dist = dist || 0;
-
+    this.parent = parent;
+    
+    this.show=function(){
+        return "{" + x + ", " + y + "}"; 
+    }
 };
 
 
@@ -50,12 +65,12 @@ function Util(){
     let MAX_VALUE = 77777;
 
 
-	// Below arrays details all 4 possible movements from a cell
+    // Below arrays details all 4 possible movements from a cell
     var row = [-1, 0, 0, 1];
     var col = [0, -1, 1, 0];
 
     // Function to check if it is possible to go to position (row, col)
-	// from current position. The function returns false if (row, col)
+    // from current position. The function returns false if (row, col)
     // is not a valid position or has value 0 or it is already visited
     function isValid(mat, visited, row, col){
         return (row >= 0) && (row < M) && (col >= 0) && (col < N)
@@ -86,42 +101,43 @@ function Util(){
             var node = q.poll();
             
             // (i, j) represents current cell and dist stores its
-			// minimum distance from the source
-			i = node.x;
-			j = node.y;
+            // minimum distance from the source
+            i = node.x;
+            j = node.y;
             var dist = node.dist;
             
             // if destination is found, update min_dist and stop
-			if (i == x && j == y)
-			{
-				min_dist = dist;
-				break;
+            if (i == x && j == y)
+            {
+                min_dist = dist;
+                break;
             }
             
 
-			// check for all 4 possible movements from current cell
-			// and enqueue each valid movement
-			for ( k = 0; k < 4; k++)
-			{
-				// check if it is possible to go to position
-				// (i + row[k], j + col[k]) from current position
-				if (isValid(mat, visited, i + row[k], j + col[k]))
-				{
-					// mark next cell as visited and enqueue it
-					visited[i + row[k]][j + col[k]] = true;
-					q.add(new Node(i + row[k], j + col[k], dist + 1));
-				}
-			}
+            // check for all 4 possible movements from current cell
+            // and enqueue each valid movement
+            for ( k = 0; k < 4; k++)
+            {
+                // check if it is possible to go to position
+                // (i + row[k], j + col[k]) from current position
+                if (isValid(mat, visited, i + row[k], j + col[k]))
+                {
+                    // mark next cell as visited and enqueue it
+                    visited[i + row[k]][j + col[k]] = true;
+                    q.add(new Node(i + row[k], j + col[k], dist + 1, node));
+                }
+            }
 
         }
 
-		if (min_dist != MAX_VALUE) {
-			alert("The shortest path from source to destination "
-							+ "has length " + min_dist);
-		}
-		else {
-			alert("Destination can't be reached from source");
-		}
+        if (min_dist != MAX_VALUE) {
+            document.write("The shortest path from source to destination "
+                            + "has length </br> " + min_dist);
+                            printPath(node);
+        }
+        else {
+            document.write("Destination can't be reached from source");
+        }
     }
 
 
@@ -129,23 +145,23 @@ function Util(){
     
 
         // input maze    
-		var  mat =
-		[
-			[ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
-			[ 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 ],
-			[ 0, 0, 1, 0, 1, 1, 1, 0, 0, 1 ],
-			[ 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 ],
-			[ 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 ],
-			[ 1, 0, 1, 1, 1, 0, 0, 1, 1, 0 ],
-			[ 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
-			[ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
-			[ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
-			[ 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 ],
+        var  mat =
+        [
+            [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
+            [ 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 ],
+            [ 0, 0, 1, 0, 1, 1, 1, 0, 0, 1 ],
+            [ 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 ],
+            [ 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 ],
+            [ 1, 0, 1, 1, 1, 0, 0, 1, 1, 0 ],
+            [ 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
+            [ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
+            [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
+            [ 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 ],
         ];
 
-		// Find shortest path from source (0, 0) to
-		// destination (7, 5)
-		BFS(mat, 0, 0, 7, 5);
+        // Find shortest path from source (0, 0) to
+        // destination (7, 5)
+        BFS(mat, 0, 0, 7, 5);
     
 
 }
